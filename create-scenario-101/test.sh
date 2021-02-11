7477d596d83c9387b0c25ff902b208865d75963e
JakeSCahill <jakecahillgh@gmail.com>
2021-02-11 15:19:02 +0000

    Reconfigure

--- create-scenario-101/test.sh
+++ create-scenario-101/test.sh
@@ -8,7 +8,11 @@ done
 
 mkdir -p /usr/local/bin/src/main/java/com/hazelcast/jet/examples/helloworld
 
+mkdir -p /usr/local/bin/src/main/resources
+
 cp /usr/local/bin/HelloWorld.class /usr/local/bin/src/main/java/com/hazelcast/jet/examples/helloworld
+
+cp /usr/local/bin/hazelcast.yaml /usr/local/bin/src/main/resources
 
 cd /usr/local/bin
 
