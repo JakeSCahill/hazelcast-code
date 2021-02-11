d05b4809140f92231ac47468840d886ad999dd36
JakeSCahill <jakecahillgh@gmail.com>
2021-02-11 18:23:12 +0000

    Add jet examples

--- create-scenario-101/test.sh
+++ create-scenario-101/test.sh
@@ -2,6 +2,10 @@
 echo "Starting Jet"
 clear
 cd /usr/local/bin
+curl -O https://github.com/hazelcast/hazelcast-jet/releases/download/v4.4/hazelcast-jet-4.4.tar.gz
+tar zxvf hazelcast-jet-4.4.tar.gz
+rm hazelcast-jet-4.4.tar.gz
+cd hazelcast-jet-4.4/bin
 chmod +x jet-start
 chmod +x jet
 chmod +x jet-stop
