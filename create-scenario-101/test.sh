8a734aa7afd436a75dd567dbad025fc19b298d0f
JakeSCahill <jakecahillgh@gmail.com>
2021-02-11 13:19:51 +0000

    Reconfigure

--- create-scenario-101/test.sh
+++ create-scenario-101/test.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-java -jar /usr/local/bin/hazelcast-jet-examples-hello-world-4.5-SNAPSHOT.jar
+clear
+while [ ! -f /usr/local/bin/hazelcast-jet-examples-hello-world-4.5-SNAPSHOT.jar ]
+do
+  clear
+  sleep .2
+done
+
+chmod 755 /usr/local/bin/hazelcast-jet-examples-hello-world-4.5-SNAPSHOT.jar; java -jar /usr/local/bin/hazelcast-jet-examples-hello-world-4.5-SNAPSHOT.jar
