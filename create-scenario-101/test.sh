392a8ddbb6afc0cdad41ad2d8adcdcc91717b144
JakeSCahill <jakecahillgh@gmail.com>
2021-02-11 14:04:35 +0000

    Reconfigure

--- create-scenario-101/test.sh
+++ create-scenario-101/test.sh
@@ -6,10 +6,8 @@ do
   sleep .2
 done
 
-export CLASSPATH= /usr/local/bin
-
 mkdir -p /usr/local/bin/com/hazelcast/jet/examples/helloworld
 
 cp /usr/local/bin/HelloWorld.class /usr/local/bin/com/hazelcast/jet/examples/helloworld
 
-chmod 755 /usr/local/bin/hazelcast-jet-examples-hello-world-4.5-SNAPSHOT.jar; java -jar /usr/local/bin/hazelcast-jet-examples-hello-world-4.5-SNAPSHOT.jar
+chmod 755 /usr/local/bin/hazelcast-jet-examples-hello-world-4.5-SNAPSHOT.jar; java -cp /usr/local/bin -jar /usr/local/bin/hazelcast-jet-examples-hello-world-4.5-SNAPSHOT.jar
