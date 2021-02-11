aa6612333fc8f34c02fcba3f10337fb637f8dd77
JakeSCahill <jakecahillgh@gmail.com>
2021-02-11 15:26:39 +0000

    Reconfigure

--- create-scenario-101/test.sh
+++ create-scenario-101/test.sh
@@ -6,14 +6,6 @@ do
   sleep .2
 done
 
-mkdir -p /usr/local/bin/src/main/java/com/hazelcast/jet/examples/helloworld
-
-mkdir -p /usr/local/bin/src/main/resources
-
-cp /usr/local/bin/HelloWorld.class /usr/local/bin/src/main/java/com/hazelcast/jet/examples/helloworld
-
-cp /usr/local/bin/hazelcast.yaml /usr/local/bin/src/main/resources
-
 cd /usr/local/bin
 
 mvn exec:java -Dexec.mainClass=com.hazelcast.jet.examples.helloworld.HelloWorld
