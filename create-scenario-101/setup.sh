240eba71d401259dd97ca0378abbb264408ee77c
JakeSCahill <jakecahillgh@gmail.com>
2021-02-11 16:23:23 +0000

    Reconfigure

---
+++ create-scenario-101/setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+clear
+wget https://github.com/hazelcast/hazelcast-jet/releases/download/v4.4/hazelcast-jet-4.4.tar.gz
+tar zxvf hazelcast-jet-4.4.tar.gz
--- create-scenario-101/test.sh
+++ create-scenario-101/test.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 clear
-while [ ! -f /usr/local/bin/hazelcast-jet/examples/hello-world/src/main/java/com/hazelcast/jet/examples/helloworld/HelloWorld.java ]
+while [ ! -f /usr/local/tmp/hazelcast-jet-4.4/LICENSE ]
 do
   clear
   sleep .2
 done
 
-cd /usr/local/bin/hazelcast-jet/examples/hello-world
-
-mvn exec:java -Dexec.mainClass=src.main.java.com.hazelcast.jet.examples.helloworld.HelloWorld
- 
+cd hazelcast-jet-4.4
+bin/jet-start
