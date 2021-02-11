e8f56984e0d7f925910a0fc27d25d335f73fc932
JakeSCahill <jakecahillgh@gmail.com>
2021-02-11 14:16:42 +0000

    Reconfigure

--- create-scenario-101/test.sh
+++ create-scenario-101/test.sh
@@ -10,4 +10,4 @@ mkdir -p /usr/local/bin/com/hazelcast/jet/examples/helloworld
 
 cp /usr/local/bin/HelloWorld.class /usr/local/bin/com/hazelcast/jet/examples/helloworld
 
-chmod 755 /usr/local/bin/hazelcast-jet-examples-hello-world-4.5-SNAPSHOT.jar; java -cp /usr/local/bin -jar /usr/local/bin/hazelcast-jet-examples-hello-world-4.5-SNAPSHOT.jar
+chmod 755 /usr/local/bin/hazelcast-jet-examples-hello-world-4.5-SNAPSHOT.jar; java -cp /usr/local/bin/hazelcast-jet-examples-hello-world-4.5-SNAPSHOT-sources.jar -jar /usr/local/bin/hazelcast-jet-examples-hello-world-4.5-SNAPSHOT.jar
