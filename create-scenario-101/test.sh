63fb94fbfcb74a4f3dfa2a9840603b213ebef45e
JakeSCahill <jakecahillgh@gmail.com>
2021-02-11 15:32:53 +0000

    Reconfigure

--- create-scenario-101/test.sh
+++ create-scenario-101/test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 clear
-while [ ! -f /usr/local/bin/src/main/java/com/hazelcast/jet/examples/helloworld/HelloWorld.java ]
+while [ ! -f /usr/local/bin/hazelcast/jet/examples/helloworld/src/main/java/com/hazelcast/jet/examples/helloworld/HelloWorld.java ]
 do
   clear
   sleep .2
 done
 
-cd /usr/local/bin
+cd /usr/local/bin/hazelcast/jet/examples/helloworld
 
 mvn exec:java -Dexec.mainClass=com.hazelcast.jet.examples.helloworld.HelloWorld
  
