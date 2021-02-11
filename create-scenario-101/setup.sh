435b8add3963e7c1da2891239aec709bb719913b
JakeSCahill <jakecahillgh@gmail.com>
2021-02-11 16:43:09 +0000

    Reconfigure

---
+++ create-scenario-101/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+clear
+mkdir -p /usr/local/tmp
+cd /usr/local/tmp
+echo "Downloading the Jet source..."
+wget https://github.com/hazelcast/hazelcast-jet/releases/download/v4.4/hazelcast-jet-4.4.tar.gz
+tar zxvf hazelcast-jet-4.4.tar.gz
+rm hazelcast-jet-4.4.tar.gz
+
+cd /usr/local/tmp/hazelcast-jet-4.4
+bin/jet-start
