ce72a820bbf1a1e3b0c572579d9de3442e0e033e
JakeSCahill <jakecahillgh@gmail.com>
2021-02-18 15:07:01 +0000

    Use full distr

--- static-queries/start-member.sh
+++ static-queries/start-member.sh
@@ -2,10 +2,10 @@
 echo "Starting Hazelcast"
 clear
 cd /usr/local/bin
-wget -4 https://oss.sonatype.org/content/repositories/snapshots/com/hazelcast/hazelcast-distribution/5.0-SNAPSHOT/hazelcast-distribution-5.0-20210217.144925-5-slim.tar.gz
-tar zxvf hazelcast-distribution-5.0-20210217.144925-5-slim.tar.gz
-rm hazelcast-distribution-5.0-20210217.144925-5-slim.tar.tar.gz
-cd hazelcast-5.0-SNAPSHOT-slim/bin
+wget -4 https://oss.sonatype.org/content/repositories/snapshots/com/hazelcast/hazelcast-distribution/5.0-SNAPSHOT/hazelcast-distribution-5.0-20210217.144925-5.tar.gz
+tar zxvf hazelcast-distribution-5.0-20210217.144925-5.tar.gz
+rm hazelcast-distribution-5.0-20210217.144925-5.tar.gz
+cd hazelcast-5.0-SNAPSHOT/bin
 chmod +x hazelcast-start
 chmod +x hazelcast
 chmod +x hazelcast-stop
