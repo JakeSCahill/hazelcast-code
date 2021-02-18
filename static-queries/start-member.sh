29cb174460f4b96abba3cc4dd6107dffa2f2fd1d
JakeSCahill <jakecahillgh@gmail.com>
2021-02-18 14:07:39 +0000

    Update example

--- static-queries/start-member.sh
+++ static-queries/start-member.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 echo "Starting Hazelcast"
 clear
-mkdir csv-dir
-printf "id\tname\tlike\n" >> csv-dir/likes.csv
-printf "1\tJerry\t13\n" >> csv-dir/likes.csv
-printf "2\tGreg\t108\n" >> csv-dir/likes.csv
-printf "3\tMary\t73\n" >> csv-dir/likes.csv
-printf "4\tJerry\t88" >> csv-dir/likes.csv
 cd /usr/local/bin
 wget -4 https://oss.sonatype.org/content/repositories/snapshots/com/hazelcast/hazelcast-distribution/5.0-SNAPSHOT/hazelcast-distribution-5.0-20210217.144925-5-slim.tar.gz
 tar zxvf hazelcast-distribution-5.0-20210217.144925-5-slim.tar.gz
@@ -16,4 +10,10 @@ chmod +x hazelcast-start
 chmod +x hazelcast
 chmod +x hazelcast-stop
 chmod +x common.sh
+mkdir csv-dir
+printf "id\tname\tlike\n" >> csv-dir/likes.csv
+printf "1\tJerry\t13\n" >> csv-dir/likes.csv
+printf "2\tGreg\t108\n" >> csv-dir/likes.csv
+printf "3\tMary\t73\n" >> csv-dir/likes.csv
+printf "4\tJerry\t88\n" >> csv-dir/likes.csv
 ./hazelcast-start
